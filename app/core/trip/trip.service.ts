a8436881ed11a3a8b8f5f61d7efdc692b4180768
Nick Papadakis <nickfin2014@gmail.com>
2025-06-19 23:31:58 -0600

    update command again

--- app/core/trip/trip.service.ts
+++ app/core/trip/trip.service.ts
@@ -48,7 +48,7 @@ const createTrip = async (
     const { data } = await DynamoTrip()
         .put({
             ...trip,
-            ...(process.env.MAGNITUDE_TEST_URL && { expireAt: getTTL(1) }),
+            ...(process.env.WEB_URL && { expireAt: getTTL(1) }),
         })
         .go()
     return data.tripId
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-    testDir: './app/e2e',
     fullyParallel: true,
     forbidOnly: Boolean(!process.env.CI),
     retries: process.env.CI ? 1 : 0,
-    workers: process.env.CI ? 2 : undefined,
+    workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
         baseURL: `${process.env.WEB_URL!}`,
@@ -14,10 +13,6 @@ export default defineConfig({
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
-        },
-        {
-            name: 'Mobile Safari',
-            use: { ...devices['iPhone 12'] },
         },
     ],
 })
